1f3c7f21becf45e5aded5df4eab833713dd4604c
Robert Pliszka <robplisz@gmail.com>
2022-05-21 17:16:44 -0400

    Finished all CRUD routes for Post model

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -6,6 +6,7 @@ router.get('/', (req, res) => {
     console.log('==================');
     Post.findAll({
         attributes: ['id', 'post_url', 'title', 'created_at'],
+        order: [['created_at', 'DESC']],
         include: [
             {
                 model: User,
@@ -68,6 +69,26 @@ router.put('/:id', (req, res) => {
         title: req.body.title
     },
     {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Delete a post
+router.delete('/:id', (req, res) => {
+    Post.destroy({
         where: {
             id: req.params.id
         }
