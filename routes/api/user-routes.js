af87de03e815870e603bd18392324b53e1d41000
Robert Pliszka <robplisz@gmail.com>
2022-05-22 18:40:12 -0400

    Added users to display what posts they had voted on

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -15,14 +15,31 @@ router.get('/', (req, res) => {
       });
 });
 
-// GET /api/user/1
+// GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
         // dont show password
         attributes: { exclude: ['password']},
         where: {
             id: req.params.id
-        }
+        },
+        include: [ 
+            {
+                model: Post,
+                attributes: [
+                    'id',
+                    'title',
+                    'post_url',
+                    'created_at'
+                ]
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: "voted_posts"
+            }
+        ]
     })
     .then(dbUserData => {
         if (!dbUserData) {
