a281c128368af2022c296732f9b0820dd9c3fcf8
Robert Pliszka <robplisz@gmail.com>
2022-05-24 18:54:21 -0400

    Finished the login and logout sessions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -65,47 +65,72 @@ router.get('/:id', (req, res) => {
 });
 
 // POST /api/users
+
 router.post('/', (req, res) => {
-    // expects {usernam: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
+      .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json(dbUserData);
+        });
+      })
+      .catch(err => {
         console.log(err);
         res.status(500).json(err);
-    });
-});
+      });
+  });
 
 // User login
 
 router.post('/login', (req, res) => {
     // expects {email: 'lernantino@gmail.com', password: 'password1234'}
     User.findOne({
-        where: {
-            email: req.body.email
-        }
+      where: {
+        email: req.body.email
+      }
     }).then(dbUserData => {
-        if(!dbUserData) {
-            res.status(400).json({ message: 'No user with that email address! '});
-            return;
-        }
-
-        // res.json({ user: dbUserData });
-
-        // verify user
-        const validPassword = dbUserData.checkPassword(req.body.password);
-        if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect password!'});
-            return;
-        }
-
-        res.json({ user: dbUserData, message: "You are now logged in!"});
-
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
     });
+  });
+
+//   logout
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
+  
 
 
 // PUT /api/user/1
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,62 +1,53 @@
-// Signup form handler
-async function signupFormHandler(event) {
+async function loginFormHandler(event) {
     event.preventDefault();
-
-    // user sugnup information from the form
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  async function signupFormHandler(event) {
+    event.preventDefault();
+  
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-
-    if(username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-        
-        // check the response status
-        if (response.ok) {
-            console.log('success')
-        } else {
-            alert(response.statusText);
-        }
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
     }
-}
-
-
-// Event listener for signup button
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-
-// login form handler
-async function loginFormHandler(event) {
-    event.preventDefault();
-
-    // login information from the form
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-
-    if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            header: { 'Content-Type': 'application/json'}
-        });
-
-        // check the response status
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
-
-// Event listiner for login button
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  }
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
