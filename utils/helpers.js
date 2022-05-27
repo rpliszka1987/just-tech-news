abc52387b291743c9436195cf0c4c875612f8f81
Robert Pliszka <robplisz@gmail.com>
2022-05-26 22:27:30 -0400

    Wrote a format_url() which shortens the url

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,6 +8,14 @@ module.exports = {
         }
 
         return word;
+    },
+    format_url: url => {
+        return url
+        .replace('http://', '')
+        .replace('https://', '')
+        .replace('www.', '')
+        .split('/')[0]
+        .split('?')[0];
     }
 }
 
