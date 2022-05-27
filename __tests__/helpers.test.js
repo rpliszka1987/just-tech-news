4b54646ff54424c3041ccca76f29e356685db51c
Robert Pliszka <robplisz@gmail.com>
2022-05-26 22:19:35 -0400

    Added test to test format_plural() from helpers

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,13 @@
-const { format_date } = require('../utils/helpers');
+const { format_date, format_plural } = require('../utils/helpers');
 
 test('format_date() returns a date string', () =>{
     const date = new Date('2020-03-20 16:12:03');
 
     expect(format_date(date)).toBe('3/20/2020');
 });
+
+test('format_plural() return a correct format', () => {
+    expect(format_plural("Tiger", 2)).toBe('Tigers');
+    expect(format_plural("Lion", 1)).toBe('Lion');
+});
+
